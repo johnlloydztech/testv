06ef778e06af90668da267305c6eb3c270ea6b66
John Lloyd Ponce <75487625+johnlloydztech@users.noreply.github.com>
2023-04-27 21:49:39 +0800

    Update Control.jsx

--- src/pages/Control.jsx
+++ src/pages/Control.jsx
@@ -49,36 +49,50 @@ const Control = () => {
   
   const handleButton1Click = () => {
     const dryingRef = ref(db, "Data/Control/Drying");
-    if (button2On === 1) {  // <-- check the value of button2On instead of dryingRef
+    if (button2On) {
       alert("Drying process is on going");
       return;
     }
-    set(ref(db, "Data/Control"), {
-      Fermentation: !button1On ? 1 : 0,
-      Drying: button2On ? 1 : 0,
-    })
-      .then(() => {
-        setButton1On(!button1On);
+    if (button1On) {
+      set(ref(db, "Data/Control"), {
+        Fermentation: 0,
+        Drying: button2On ? 1 : 0,
       })
-      .catch((error) => {
-        console.error("Error updating database: ", error);
-      });
+        .then(() => {
+          setButton1On(false);
+        })
+        .catch((error) => {
+          console.error("Error updating database: ", error);
+        });
+    } else {
+      set(ref(db, "Data/Control"), {
+        Fermentation: 1,
+        Drying: button2On ? 1 : 0,
+      })
+        .then(() => {
+          setButton1On(true);
+        })
+        .catch((error) => {
+          console.error("Error updating database: ", error);
+        });
+    }
   };
+  
   
   
   const handleButton2Click = () => {
     const dryingRef = ref(db, "Data/Control/Drying");
     const fermentationRef = ref(db, "Data/Control/Fermentation");
-    if (button2On !== 1) { // <-- check the value of button2On instead of dryingRef
+    if (!button2On) { // check if button2On is false
       return;
     }
-    if (button2On === 1) { // <-- check the value of button2On instead of dryingRef
+    if (button2On) { // check if button2On is true
       const stopDrying = window.confirm(
         "Are you sure you want to stop the drying process?"
       );
       if (stopDrying) {
         set(ref(db, "Data/Control"), {
-          Fermentation: fermentationRef === 1 ? 1 : 0,
+          Fermentation: button1On ? 1 : 0,
           Drying: 0,
         }).then(() => {
           setButton2On(false);
